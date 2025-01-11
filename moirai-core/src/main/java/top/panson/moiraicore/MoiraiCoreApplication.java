1a86cc3d7f631c91b8cc188419fcc07e64039b88
Panson <PansonPanson@163.com>
2025-01-11 16:26:41 +0800

    增加 moirai-core module

---
+++ moirai-core/src/main/java/top/panson/moiraicore/MoiraiCoreApplication.java
@@ -0,0 +1,13 @@
+package top.panson.moiraicore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoiraiCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoiraiCoreApplication.class, args);
+    }
+
+}
---
+++ moirai-core/src/test/java/top/panson/moiraicore/MoiraiCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package top.panson.moiraicore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoiraiCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
