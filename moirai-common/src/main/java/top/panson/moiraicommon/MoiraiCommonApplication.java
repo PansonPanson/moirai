2b1d897e8c35dda3a299087eff164c07d35e3ff8
Panson <PansonPanson@163.com>
2025-02-12 17:13:18 +0800

    增加 module：common

---
+++ moirai-common/src/main/java/top/panson/moiraicommon/MoiraiCommonApplication.java
@@ -0,0 +1,13 @@
+package top.panson.moiraicommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoiraiCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoiraiCommonApplication.class, args);
+    }
+
+}
---
+++ moirai-common/src/test/java/top/panson/moiraicommon/MoiraiCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package top.panson.moiraicommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoiraiCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
