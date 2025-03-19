505b67486c6055f38793e8d06631037becfca9dd
Panson <PansonPanson@163.com>
2025-03-19 18:09:24 +0800

    增加可扩展的线程池与插件体系

---
+++ moirai-core/src/main/java/top/panson/moiraicore/AbstractTaskTimerPlugin.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.panson.moiraicore;
+
+
+
+import java.util.Optional;
+
+
+
+public abstract class AbstractTaskTimerPlugin implements ExecuteAwarePlugin {
+
+
+    private final ThreadLocal<Long> startTimes = new ThreadLocal<>();
+
+
+    //任务开始执行之前会执行这个方法，在这个方法中把任务开始执行时间放到线程本地map中
+    @Override
+    public final void beforeExecute(Thread thread, Runnable runnable) {
+        startTimes.set(currentTime());
+    }
+
+
+    //该方法会在任务执行之后被调用
+    @Override
+    public final void afterExecute(Runnable runnable, Throwable throwable) {
+        try {
+            //从线程本地map中得到任务的开始时间
+            Optional.ofNullable(startTimes.get())
+                    //计算出耗时时间
+                    .map(startTime -> currentTime() - startTime)
+                    //交给processTaskTime方法处理，这里就会来到具体的子类的中
+                    //也就是TaskTimeoutNotifyAlarmPlugin类中
+                    .ifPresent(this::processTaskTime);
+        } finally {
+            //清除线程本地map
+            startTimes.remove();
+        }
+    }
+
+    /**
+     * Get the current time.
+     *
+     * @return current time
+     */
+    protected long currentTime() {
+        return SystemClock.now();
+    }
+
+    /**
+     * Processing the execution time of the task.
+     *
+     * @param taskExecuteTime execute time of task
+     */
+    protected abstract void processTaskTime(long taskExecuteTime);
+}
---
+++ moirai-core/src/main/java/top/panson/moiraicore/DefaultThreadPoolPluginRegistrar.java
@@ -0,0 +1,35 @@
+package top.panson.moiraicore;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class DefaultThreadPoolPluginRegistrar implements ThreadPoolPluginRegistrar {
+
+    /**
+     * Execute time out
+     */
+    private long executeTimeOut;
+
+    /**
+     * Await termination millis
+     */
+    private long awaitTerminationMillis;
+
+    /**
+     * Create and register plugin for the specified thread-pool instance.
+     *
+     * @param support thread pool plugin manager delegate
+     */
+    @Override
+    public void doRegister(ThreadPoolPluginSupport support) {
+        support.register(new TaskDecoratorPlugin());
+        support.register(new TaskTimeoutNotifyAlarmPlugin(support.getThreadPoolId(), executeTimeOut, support.getThreadPoolExecutor()));
+        support.register(new TaskRejectCountRecordPlugin());
+        support.register(new TaskRejectNotifyAlarmPlugin());
+        support.register(new ThreadPoolExecutorShutdownPlugin(awaitTerminationMillis));
+    }
+}
