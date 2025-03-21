2f9e005d990d2802060e860be4301d91cede58be
Panson <PansonPanson@163.com>
2025-03-21 11:24:05 +0800

    初始服务端模块

--- moirai-server/src/test/java/top/panson/moiraicore/MoiraiCoreApplicationTests.java
+++ moirai-server/src/test/java/top/panson/moiraicore/MoiraiCoreApplicationTests.java
@@ -1,10 +1,10 @@
-package top.panson.moiraicommon;
+package top.panson.moiraicore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MoiraiCommonApplicationTests {
+class MoiraiCoreApplicationTests {
 
     @Test
     void contextLoads() {
